a5a7bbee634392bd011ce31c8acf64e344660b8f
Martin <martinbibyakov@gmail.com>
2020-05-13 15:25:17 +0200

    map token added, netlify deploy ready

--- src/settings/mapSettings.js
+++ src/settings/mapSettings.js
@@ -1,6 +1,8 @@
 export const initMap = {
   tileLayer: {
-    url: "https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png",
+    token: "",
+    url:
+      "https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png?access_token=pk.eyJ1Ijoic3RpbGxoYXZlbnRmb3VuZCIsImEiOiJja2E1ZDFvcDIwbHlvM29vYTRsamhkcHk1In0.gMS4-Yw_yXPptDjvJBqgkw",
     config: {
       detectRetina: true,
       maxZoom: 16,
@@ -11,6 +13,7 @@ export const initMap = {
   urls: {
     mailIcon:
       "https://cdn4.iconfinder.com/data/icons/mail-linefilled/512/email_mail__letter__internet__envelope__chat__navigation_-512.png",
-    deviceIcon: "https://cdn1.iconfinder.com/data/icons/maps-and-navigation-free/32/Maps_Maps_Navigation_Gps_Pin_Location-02-512.png"
+    deviceIcon:
+      "https://cdn1.iconfinder.com/data/icons/maps-and-navigation-free/32/Maps_Maps_Navigation_Gps_Pin_Location-02-512.png",
   },
 };
